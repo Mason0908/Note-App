d0ecb57478b4d8113eb45a88a41c21355e51daab
Diane-Ding <diane7ding@gmail.com>
2022-03-09 22:07:23 -0500

    Demo2 Tests update
    UI tests for add note and folder. Split Unit Tests by notes and folders. Integration Tests of data classes note and folder.

--- noteapp/app/src/test/java/com/example/noteapp/FolderNoteTest.kt
+++ noteapp/app/src/test/java/com/example/noteapp/FolderNoteTest.kt
@@ -34,7 +34,25 @@ internal class FolderNoteTest {
         for (i in 0 .. 49) {
             folders.add( Folder(i, listOf(), "${i+1}", 1, null))
         }
+        assertEquals(50, folders.size)
+        for (i in 0 .. 49) {
+            folders.removeAt(0)
+        }
+        assertEquals(0, folders.size)
+    }
+
+    @Test
+    fun add_note_check_in_folder() {
+        assertEquals(0, noteBoard.size)
+        var listOfID = mutableListOf<Int>()
+        for (i in 0 .. 49) {
+            noteBoard.add(Note(i, null, "$i",
+                "body $i", false, null, 1, null, null))
+            listOfID.add(i)
+        }
         assertEquals(50, noteBoard.size)
+        val folder = Folder(0,  listOfID, "f1", 1, null)
+        assertEquals(50, folder.notesId.size)
         for (i in 0 .. 49) {
             noteBoard.removeAt(0)
         }
